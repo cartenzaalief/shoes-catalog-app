e71ff05107050cd71ad47fbef3adf597e7820dbe
cartenzaalief <cartenzaalief@gmail.com>
2026-04-11 18:25:46 +0700

    feat: partner slider homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 import CarouselSection from "@/components/public/CarouselSection";
 import NewArrivals from "@/components/public/new-arrivals/NewArrivals";
+import PartnersSlider from "@/components/public/PartnerSlider";
 import SubcategoryGrid from "@/components/public/subcategory-grid/SubcategoryGrid";
-import { getCarouselSlides } from "@/lib/dal/homepage";
+import { getCarouselSlides, getPartners } from "@/lib/dal/homepage";
 import { getNewArrivals } from "@/lib/dal/products";
 import { getAllSubcategoriesWithCategories } from "@/lib/dal/subcategories";
 
@@ -9,12 +10,14 @@ export default async function Home() {
   const slides = await getCarouselSlides();
   const products = await getNewArrivals();
   const subcategories = await getAllSubcategoriesWithCategories();
+  const partners = await getPartners();
 
   return (
     <div className="w-full">
       <CarouselSection slides={slides} />
       <NewArrivals products={products} />
       <SubcategoryGrid subcategories={subcategories} />
+      <PartnersSlider partners={partners} />
     </div>
   );
 }
